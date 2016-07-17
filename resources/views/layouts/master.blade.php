be2aad8bc7e4d61da3e07cb5547fbbe0cbea34d1
Manthan Sharma <hitechmanthan@gmail.com>
2016-07-17 13:09:52 +0530

    New design

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,78 +2,474 @@
 <html>
 <head>
 	<title>Deals4U | Hackathon</title>
-	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<link rel="stylesheet" type="text/css" href="material/dist/css/bootstrap-material-design.min.css">
-	<link rel="stylesheet" type="text/css" href="material/dist/css/ripples.css">
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/foundation.css">
+	<link href='http://fonts.googleapis.com/css?family=Ubuntu:400,300,700' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="css/flexslider.css">
+	<link rel="stylesheet" href="css/main.css">
+	<link rel="stylesheet" href="css/mq-32.css" media="screen and (min-width: 32.5em)"/>
+	<link rel="stylesheet" href="css/mq-48.css" media="screen and (min-width: 48em)"/>
+	<link rel="stylesheet" href="css/mq-67.css" media="screen and (min-width: 67.5em)"/>
+	<script src="js/vendor/modernizr-2.6.2.min.js"></script>
 
 </head>
 <body>
-<div class="container">
-<div class="navbar navbar-warning">
-  <div class="container">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-warning-collapse">
-        <span class="glyphicon glyphicon-search"></span>
-      </button>
-      <a class="navbar-brand" href="javascript:void(0)">Deals4You</a>
-    </div>
-    <div class="navbar-collapse collapse navbar-warning-collapse">
-      <!--ul class="nav navbar-nav">
-        <li class="active"><a href="javascript:void(0)">Active</a></li>
-        <li><a href="javascript:void(0)">Link</a></li>
-        <li class="dropdown">
-          <a href="bootstrap-elements.html" data-target="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown
-            <b class="caret"></b></a>
-          <ul class="dropdown-menu">
-            <li><a href="javascript:void(0)">Action</a></li>
-            <li><a href="javascript:void(0)">Another action</a></li>
-            <li><a href="javascript:void(0)">Something else here</a></li>
-            <li class="divider"></li>
-            <li class="dropdown-header">Dropdown header</li>
-            <li><a href="javascript:void(0)">Separated link</a></li>
-            <li><a href="javascript:void(0)">One more separated link</a></li>
-          </ul>
-        </li>
-      </ul-->
-      <ul style="margin-right:15px;" class="nav navbar-nav navbar-right">
-            <li><a href="#search"><span class="glyphicon glyphicon-search">&nbsp;Search</span></a></li>
-      </ul>
+<div class="page home">
 
-      <!--ul class="nav navbar-nav navbar-right">
-        <li><a href="javascript:void(0)">Link</a></li>
-        <li class="dropdown">
-          <a href="bootstrap-elements.html" data-target="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown
-            <b class="caret"></b></a>
-          <ul class="dropdown-menu">
-            <li><a href="javascript:void(0)">Action</a></li>
-            <li><a href="javascript:void(0)">Another action</a></li>
-            <li><a href="javascript:void(0)">Something else here</a></li>
-            <li class="divider"></li>
-            <li><a href="javascript:void(0)">Separated link</a></li>
-          </ul>
-        </li>
-      </ul-->
+	<header role="banner" class="cf">
 
-    </div>
-  </div>
+		<div class="top-wrap">
+			<div class="row">
+				<div class="large-3 columns">
+					<div class="logo">
+						<a href="index.html">Couponizer</a>
+					</div>
+				</div>
+				<nav class="large-6 columns">
+					<ul class="rr main-menu">
+						<li class="current"><a href="index.html">Home</a></li>
+						<li><a href="blog.html">Blog</a></li>
+						<li>
+							<a href="search-results.html">Extra</a>
+							<ul>
+								<li><a href="company.html">Company</a></li>
+								<li><a href="404.html">404 page</a></li>
+								<li><a href="no-results.html">No results</a></li>
+								<li><a href="search-results.html">Search results</a></li>
+							</ul>
+						</li>
+						<li><a href="coupons.html">Coupons</a></li>
+						<li><a href="post.html">Post</a></li>
+					</ul>
+				</nav>
+				<div class="large-3 columns">
+					<div class="account cf">
+						<a href="#" class="input button blue tertiary icon plus">Join us</a>
+						<a href="#" class="input button transparent tertiary">Log in</a>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="search-wrap stripe-white">
+			<div class="row">
+				<form action="http://teothemes.com/html/Couponizer/search-results.html">
+					<div class="small-9 large-10 columns">
+						<label class="search-box">
+							<input type="text" class="input field primary" placeholder="Search listing">
+							<a href="#" class="icon clear ir" id="clear-sb">Clear</a>
+						</label>
+					</div>
+					<div class="small-3 large-2 columns">
+						<input type="submit" class="input button primary red" value="Search">
+					</div>
+				</form>
+			</div>
+		</div>
+	</header>
+
+	<div role="main">
+
+		<div class="stripe-regular items-carousel-wrap">
+			<div class="row">
+				<div class="column">
+					<h2>New Coupons</h2>
+				</div>
+			</div>
+			<div class="row collapse">
+				<div class="column">
+					<div class="items-carousel flexslider">
+						<ul class="rr slides">
+							<li>
+								<div class="wrapper-3 item-thumb">
+									<div class="top">
+										<figure>
+											<img src="img/ph/200x110/1.jpg" alt="">
+										</figure>
+										<h2 class="alt"><a href="#">Cranium Disney Family Edition at Amazon</a></h2>
+									</div>
+									<div class="bottom">
+										<p class="value secondary">$30 OFF</p>
+										<h6>31 days left</h6>
+										<a href="#" class="input button red secondary">Learn more</a>
+									</div>
+								</div>
+							</li>
+							<li>
+								<div class="wrapper-3 item-thumb">
+									<div class="top">
+										<figure>
+											<img src="img/ph/200x110/2.jpg" alt="">
+										</figure>
+										<h2 class="alt"><a href="#">Popular Marvel Movies Titles on Amazon</a></h2>
+									</div>
+									<div class="bottom">
+										<p class="value secondary">$30 OFF</p>
+										<h6>5 days left</h6>
+										<a href="#" class="input button red secondary">Learn more</a>
+									</div>
+								</div>
+							</li>
+							<li>
+								<div class="wrapper-3 item-thumb">
+									<div class="top">
+										<figure>
+											<img src="img/ph/200x110/3.jpg" alt="">
+										</figure>
+										<h2 class="alt"><a href="#">GoDaddy yearly hosting offer</a></h2>
+									</div>
+									<div class="bottom">
+										<p class="value secondary">$30 OFF</p>
+										<h6>43 days left</h6>
+										<a href="#" class="input button red secondary">Learn more</a>
+									</div>
+								</div>
+							</li>
+							<li>
+								<div class="wrapper-3 item-thumb">
+									<div class="top">
+										<figure>
+											<img src="img/ph/200x110/4.jpg" alt="">
+										</figure>
+										<h2 class="alt"><a href="#">InterSport May Orders</a></h2>
+									</div>
+									<div class="bottom">
+										<p class="value secondary">$30 OFF</p>
+										<h6>22 days left</h6>
+										<a href="#" class="input button red secondary">Learn more</a>
+									</div>
+								</div>
+							</li>
+							<li>
+								<div class="wrapper-3 item-thumb">
+									<div class="top">
+										<figure>
+											<img src="img/ph/200x110/1.jpg" alt="">
+										</figure>
+										<h2 class="alt"><a href="#">Nikon cameras</a></h2>
+									</div>
+									<div class="bottom">
+										<p class="value secondary">$20 OFF</p>
+										<h6>12 days left</h6>
+										<a href="#" class="input button red secondary">Learn more</a>
+									</div>
+								</div>
+							</li>
+							<li>
+								<div class="wrapper-3 item-thumb">
+									<div class="top">
+										<figure>
+											<img src="img/ph/200x110/1.jpg" alt="">
+										</figure>
+										<h2 class="alt"><a href="#">Cranium Disney Family Edition at Amazon</a></h2>
+									</div>
+									<div class="bottom">
+										<p class="value secondary">$30 OFF</p>
+										<h6>2 days left</h6>
+										<a href="#" class="input button red secondary">Learn more</a>
+									</div>
+								</div>
+							</li>
+							<li>
+								<div class="wrapper-3 item-thumb">
+									<div class="top">
+										<figure>
+											<img src="img/ph/200x110/2.jpg" alt="">
+										</figure>
+										<h2 class="alt"><a href="#">Popular Marvel Movies Titles on Amazon</a></h2>
+									</div>
+									<div class="bottom">
+										<p class="value secondary">$30 OFF</p>
+										<h6>4 days left</h6>
+										<a href="#" class="input button red secondary">Learn more</a>
+									</div>
+								</div>
+							</li>
+							<li>
+								<div class="wrapper-3 item-thumb">
+									<div class="top">
+										<figure>
+											<img src="img/ph/200x110/3.jpg" alt="">
+										</figure>
+										<h2 class="alt"><a href="#">GoDaddy yearly hosting offer</a></h2>
+									</div>
+									<div class="bottom">
+										<p class="value secondary">$30 OFF</p>
+										<h6>17 days left</h6>
+										<a href="#" class="input button red secondary">Learn more</a>
+									</div>
+								</div>
+							</li>
+							<li>
+								<div class="wrapper-3 item-thumb">
+									<div class="top">
+										<figure>
+											<img src="img/ph/200x110/4.jpg" alt="">
+										</figure>
+										<h2 class="alt"><a href="#">InterSport May Orders</a></h2>
+									</div>
+									<div class="bottom">
+										<p class="value secondary">$30 OFF</p>
+										<h6>12 days left</h6>
+										<a href="#" class="input button red secondary">Learn more</a>
+									</div>
+								</div>
+							</li>
+							<li>
+								<div class="wrapper-3 item-thumb">
+									<div class="top">
+										<figure>
+											<img src="img/ph/200x110/1.jpg" alt="">
+										</figure>
+										<h2 class="alt"><a href="#">Nikon cameras</a></h2>
+									</div>
+									<div class="bottom">
+										<p class="value secondary">$20 OFF</p>
+										<h6>25 days left</h6>
+										<a href="#" class="input button red secondary">Learn more</a>
+									</div>
+								</div>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="stripe-white big items-carousel-wrap">
+			<div class="row">
+				<div class="column">
+					<h2>Popular Coupons</h2>
+				</div>
+			</div>
+			<div class="row collapse">
+				<div class="column">
+					<div class="items-carousel flexslider">
+						<ul class="rr slides">
+							<li>
+								<div class="wrapper-3 item-thumb">
+									<div class="top">
+										<figure>
+											<img src="img/ph/200x110/1.jpg" alt="">
+										</figure>
+										<h2 class="alt"><a href="#">Sneakers and Athletic Shoes </a></h2>
+									</div>
+									<div class="bottom">
+										<p class="value secondary">$10 OFF</p>
+										<h6>6 days left</h6>
+										<a href="#" class="input button red secondary">Learn more</a>
+									</div>
+								</div>
+							</li>
+							<li>
+								<div class="wrapper-3 item-thumb">
+									<div class="top">
+										<figure>
+											<img src="img/ph/200x110/2.jpg" alt="">
+										</figure>
+										<h2 class="alt"><a href="#">Shirley Temple DVDs at Amazon</a></h2>
+									</div>
+									<div class="bottom">
+										<p class="value secondary">62% OFF</p>
+										<h6>3 days left</h6>
+										<a href="#" class="input button red secondary">Learn more</a>
+									</div>
+								</div>
+							</li>
+							<li>
+								<div class="wrapper-3 item-thumb">
+									<div class="top">
+										<figure>
+											<img src="img/ph/200x110/3.jpg" alt="">
+										</figure>
+										<h2 class="alt"><a href="#">Paper Mario: Sticker Star for Nintendo 3DS</a></h2>
+									</div>
+									<div class="bottom">
+										<p class="value secondary">50% OFF</p>
+										<h6>18 days left</h6>
+										<a href="#" class="input button red secondary">Learn more</a>
+									</div>
+								</div>
+							</li>
+							<li>
+								<div class="wrapper-3 item-thumb">
+									<div class="top">
+										<figure>
+											<img src="img/ph/200x110/4.jpg" alt="">
+										</figure>
+										<h2 class="alt"><a href="#">Canon PowerShot SX150 Digital Camera</a></h2>
+									</div>
+									<div class="bottom">
+										<p class="value secondary">$25 OFF</p>
+										<h6>1 day left</h6>
+										<a href="#" class="input button red secondary">Learn more</a>
+									</div>
+								</div>
+							</li>
+							<li>
+								<div class="wrapper-3 item-thumb">
+									<div class="top">
+										<figure>
+											<img src="img/ph/200x110/1.jpg" alt="">
+										</figure>
+										<h2 class="alt"><a href="#">SOUL by Ludacris HD Headphones</a></h2>
+									</div>
+									<div class="bottom">
+										<p class="value secondary">67% OFF</p>
+										<h6>9 days left</h6>
+										<a href="#" class="input button red secondary">Learn more</a>
+									</div>
+								</div>
+							</li>
+							<li>
+								<div class="wrapper-3 item-thumb">
+									<div class="top">
+										<figure>
+											<img src="img/ph/200x110/1.jpg" alt="">
+										</figure>
+										<h2 class="alt"><a href="#">Sneakers and Athletic Shoes </a></h2>
+									</div>
+									<div class="bottom">
+										<p class="value secondary">$10 OFF</p>
+										<h6>22 days left</h6>
+										<a href="#" class="input button red secondary">Learn more</a>
+									</div>
+								</div>
+							</li>
+							<li>
+								<div class="wrapper-3 item-thumb">
+									<div class="top">
+										<figure>
+											<img src="img/ph/200x110/2.jpg" alt="">
+										</figure>
+										<h2 class="alt"><a href="#">Shirley Temple DVDs at Amazon</a></h2>
+									</div>
+									<div class="bottom">
+										<p class="value secondary">62% OFF</p>
+										<h6>5 days left</h6>
+										<a href="#" class="input button red secondary">Learn more</a>
+									</div>
+								</div>
+							</li>
+							<li>
+								<div class="wrapper-3 item-thumb">
+									<div class="top">
+										<figure>
+											<img src="img/ph/200x110/3.jpg" alt="">
+										</figure>
+										<h2 class="alt"><a href="#">Paper Mario: Sticker Star for Nintendo 3DS</a></h2>
+									</div>
+									<div class="bottom">
+										<p class="value secondary">50% OFF</p>
+										<h6>2 days left</h6>
+										<a href="#" class="input button red secondary">Learn more</a>
+									</div>
+								</div>
+							</li>
+							<li>
+								<div class="wrapper-3 item-thumb">
+									<div class="top">
+										<figure>
+											<img src="img/ph/200x110/4.jpg" alt="">
+										</figure>
+										<h2 class="alt"><a href="#">Canon PowerShot SX150 Digital Camera</a></h2>
+									</div>
+									<div class="bottom">
+										<p class="value secondary">$25 OFF</p>
+										<h6>28 days left</h6>
+										<a href="#" class="input button red secondary">Learn more</a>
+									</div>
+								</div>
+							</li>
+							<li>
+								<div class="wrapper-3 item-thumb">
+									<div class="top">
+										<figure>
+											<img src="img/ph/200x110/1.jpg" alt="">
+										</figure>
+										<h2 class="alt"><a href="#">SOUL by Ludacris HD Headphones</a></h2>
+									</div>
+									<div class="bottom">
+										<p class="value secondary">67% OFF</p>
+										<h6>12 days left</h6>
+										<a href="#" class="input button red secondary">Learn more</a>
+									</div>
+								</div>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+	<footer role="contentinfo">
+		<div class="stripe-dark pre-footer-wrap">
+			<div class="row">
+				<div class="large-4 columns">
+					<h3 class="alt">About</h3>
+					<p>
+						Ut sodales ultrices metus, at molestie tellus cursus quis. Cras placerat bibendum quam sed
+						facilisis. Suspendisse dapibus eros eget tellus tempor lacinia. Cras ornare lorem sit amet arcu
+						accumsan ut tristique est fringilla.
+					</p>
+				</div>
+				<div class="small-6 large-4 large-offset-1 columns">
+					<h3 class="alt">Recent Tweets</h3>
+					<div class="tweet">
+						<p>
+							Couponizer is a multi-purpose PSD template. Created by #bestwebsoft - hand-made websites
+							from great guys http://t.co/HXvWw
+						</p>
+						<a href="#" class="posted">posted 5 minutes ago</a>
+					</div>
+				</div>
+				<nav class="small-6 large-2 large-offset-1 columns">
+					<h3 class="alt">Menu</h3>
+					<ul class="rr footer-menu">
+						<li><a href="#">Home</a></li>
+						<li><a href="#">About</a></li>
+						<li><a href="#">Terms &amp; Conditions</a></li>
+						<li><a href="#">Privacy Policy</a></li>
+						<li><a href="#">Jobs</a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+
+		<div class="stripe-darker footer-wrap">
+			<div class="row">
+				<div class="large-3 columns">
+					<div class="logo">
+						<a href="#">Couponizer</a>
+					</div>
+				</div>
+				<div class="large-6 columns">
+					<div class="copyright">Copyright &copy; 2013, developed by <a
+								href="http://teothemes.com/">TeoThemes</a>. All rights reserved.
+					</div>
+				</div>
+				<div class="large-3 columns">
+					<ul class="rr social">
+						<li><a href="#" class="ir tw">Twitter</a></li>
+						<li><a href="#" class="ir in">LinkedIn</a></li>
+						<li><a href="#" class="ir fb">Facebook</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+
+	</footer>
+
+
 </div>
-<div id="search">
-    <button type="button" class="close">×</button>
-    <form method="GET" action="/search">
-        <input type="search" name="s" value="" placeholder="Search Products..." />
-        <button type="submit" class="btn btn-primary">Search</button>
-    </form>
-</div>
-<div class="container">
-	@yield('content')
-</div>
-</div>
-<script src="jquery/jquery.min.js"></script>
-<script src="bootstrap/js/bootstrap.min.js"></script>
-<script src="material/dist/js/material.js"></script>
-<script src="material/dist/js/ripples.js"></script>
-<script src="js/script.js"></script>
+
+<!-- <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script> -->
+<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
+<script src="js/plugins.js"></script>
+<script src="js/main.js"></script>
+</body>
 
 </body>
 </html>
